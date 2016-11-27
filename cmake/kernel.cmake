abe4a6d6161f3d1f2d56026914bf6f75d2bba014
Tomi Äijö <tomi.aijo@parmance.com>
2016-11-27 19:21:54 +0200

    Add missing tests to test lists

--- cmake/kernel.cmake
+++ cmake/kernel.cmake
@@ -2,7 +2,7 @@
 set (BRIG_DIR "${CMAKE_SOURCE_DIR}/src/kernels")
 
 ## Included source files.
-set (BRIG_FILES agent_dispatch.brig depend_module1.brig depend_module2.brig global_vector_copy.brig global_agent_vector_copy.brig group_memory.brig init_data.brig memory_ops.brig mixed_scope.brig module_scope.brig no_op2.brig no_op.brig no_op_small.brig private_memory.brig program_scope.brig readonly_vector_copy.brig signal_operations.brig vector_copy.brig verify_image_region.brig kernarg_align.brig recursive_func.brig control_device.brig)
+set (BRIG_FILES agent_dispatch.brig depend_module1.brig depend_module2.brig global_vector_copy.brig global_vector_copy_base_large.brig global_agent_vector_copy.brig group_memory.brig init_data.brig memory_ops.brig mixed_scope.brig module_scope.brig no_op2.brig no_op.brig no_op_small.brig private_memory.brig program_scope.brig readonly_vector_copy.brig signal_operations.brig vector_copy.brig verify_image_region.brig kernarg_align.brig recursive_func.brig control_device.brig global_agent_vector_copy_base_large.brig no_op_base_large.brig recursive_func_base_large.brig program_scope_base_large.brig module_scope_base_large.brig mixed_scope_base_large.brig vector_copy_base_large.brig global_vector_copy_base_large.brig readonly_vector_copy_base_large.brig)
 
 add_custom_target(copy-brig-files ALL)
 
